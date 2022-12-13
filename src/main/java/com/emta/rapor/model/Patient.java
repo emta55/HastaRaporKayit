9912c034ceb5b6c177c52f0534e112ab893197f3
Emre Yılmaz <55348820+emta55@users.noreply.github.com>
2022-12-14 02:56:35 +0300

    Update Patient.java

--- src/main/java/com/emta/rapor/model/Patient.java
+++ src/main/java/com/emta/rapor/model/Patient.java
@@ -1,2 +1,75 @@
-package com.emta.rapor.model;public class Patient {
+package com.emta.rapor.model;
+
+import javax.persistence.*;
+import java.awt.*;
+
+@Entity
+@Table(name = "patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long dosyaNo;
+    private String isim;
+    private String soyIsim;
+    private String tcKimlik;
+    private String taniBasligi;
+    private String taniDetaylari;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDosyaNo() {
+        return dosyaNo;
+    }
+
+    public void setDosyaNo(Long dosyaNo) {
+        this.dosyaNo = dosyaNo;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyIsim() {
+        return soyIsim;
+    }
+
+    public void setSoyIsim(String soyIsim) {
+        this.soyIsim = soyIsim;
+    }
+
+    public String getTcKimlik() {
+        return tcKimlik;
+    }
+
+    public void setTcKimlik(String tcKimlik) {
+        this.tcKimlik = tcKimlik;
+    }
+
+    public String getTaniBasligi() {
+        return taniBasligi;
+    }
+
+    public void setTaniBasligi(String taniBasligi) {
+        this.taniBasligi = taniBasligi;
+    }
+
+    public String getTaniDetaylari() {
+        return taniDetaylari;
+    }
+
+    public void setTaniDetaylari(String taniDetaylari) {
+        this.taniDetaylari = taniDetaylari;
+    }
+
 }
