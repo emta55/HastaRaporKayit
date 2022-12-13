f481dbb16e730ebed95d540481dedb220116613c
Emre Yılmaz <55348820+emta55@users.noreply.github.com>
2022-12-14 02:57:06 +0300

    Update PatientServices.java

--- src/main/java/com/emta/rapor/services/PatientServices.java
+++ src/main/java/com/emta/rapor/services/PatientServices.java
@@ -1,2 +1,29 @@
-package com.emta.rapor.services;public class PatientServices {
+package com.emta.rapor.services;
+
+import com.emta.rapor.model.Patient;
+import com.emta.rapor.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientServices {
+
+    @Autowired
+    private PatientRepository repo;
+
+    public List<Patient> listAll(){
+        return repo.findAll();
+    }
+    public void save(Patient patient){
+        repo.save(patient);
+    }
+    public Patient get(Long id){
+        return repo.findById(id).get();
+    }
+    public void delete(Long id){
+        repo.deleteById(id);
+    }
+
 }
